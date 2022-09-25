24531fca1959cc4432fceba66577f4ada530b9b6
GEORGET <GEORGET@LAPTOP-EL8P4QVP>
2022-09-25 09:50:58 -0500

    Agregar archivos de proyecto.

---
+++ PrimerProyectoN6DPA/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyectoN6DPA
+{
+    internal class Persona
+    {
+        public int dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public int edad { get; set; }
+
+        public Persona()
+        {
+                
+        }
+
+
+        public Persona (int dni, string nombreCompleto, int edad)
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.edad = edad;
+
+    }
+}
